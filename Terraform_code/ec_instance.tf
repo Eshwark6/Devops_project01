f9e5b12195919cd8aeb4eeb0c69d20e2a55228fb
Eshwark6 <eshwark2399@gmail.com>
2025-02-21 19:44:09 +0530

    Modified version

--- Terraform_code/ec_instance.tf
+++ Terraform_code/ec_instance.tf
@@ -3,18 +3,18 @@ provider "aws" {
   
 }
 
-resource "aws_instance" "Server-1" {
+resource "aws_instance" "Server-2" {
     ami = "ami-053a45fff0a704a47"
     instance_type = "t2.micro"
     key_name = "ec2"
-    vpc_security_group_ids = [ aws_security_group.Devops-SG.id ]
+    vpc_security_group_ids = [ aws_security_group.Prod-SG.id ]
     tags = {
-        Name = "Server-1"
+        Name = "Server-2"
     }
 }
 
-resource "aws_security_group" "Devops-SG" {
-  name        = "Dev-SG"
+resource "aws_security_group" "Prod-SG" {
+  name        = "Prod-SG"
   description = "This group will provide access to EC2"
 
   tags = {
@@ -23,7 +23,7 @@ resource "aws_security_group" "Devops-SG" {
 }
 
 resource "aws_vpc_security_group_ingress_rule" "allow_Ingress" {
-  security_group_id = aws_security_group.Devops-SG.id
+  security_group_id = aws_security_group.Prod-SG.id
   from_port         = 22
   ip_protocol       = "tcp"
   to_port           = 22
@@ -31,7 +31,7 @@ resource "aws_vpc_security_group_ingress_rule" "allow_Ingress" {
 }
 
 resource "aws_vpc_security_group_egress_rule" "allow_egress" {
-  security_group_id = aws_security_group.Devops-SG.id
+  security_group_id = aws_security_group.Prod-SG.id
   cidr_ipv4         = "0.0.0.0/0"
   ip_protocol       = "-1" # semantically equivalent to all ports
 }
